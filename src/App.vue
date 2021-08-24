e9527e78201f6a6d68dd8cde04ccf57151c6389c
Shawn Kiewel <smkiewel@uga.edu>
2021-08-24 13:43:36 -0400

    Add button

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app" class="container-fluid">
-    <h1>Unique Sum Helpers</h1>
+    <div class="row">
+      <div class="col-12 col-md-auto"><h1>Unique Sum Helpers</h1></div>
+      <div class="col-12 col-md-6 button-div"><add-helper-button></add-helper-button></div>
+    </div>
   </div>
 </template>
 
 <script>
   import Vue from 'vue'
   import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+  import AddHelperButton from './components/AddHelperButton.vue'
 
   import 'bootstrap/dist/css/bootstrap.css'
   import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -17,7 +21,21 @@
 export default {
   name: 'App',
   components: {
-
+    AddHelperButton
+  },
+  methods: {
+    getNextHelperId: function() {
+      return 'helper-' + ++(this.count)
+    },
+    addHelper: function(){
+      this.helpers.push({id: this.getNextHelperId()})
+    },
+  },
+  data: function() {
+    return {
+      helpers: [],
+      count: 0
+    }
   }
 }
 </script>
